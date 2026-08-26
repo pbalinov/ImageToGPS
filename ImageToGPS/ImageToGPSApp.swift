8c29b98786c9911ce7f1cc86dbc98936febf956a
Plamen Balinov <pbalinov@icloud.com>
2026-08-26 13:24:19 +0300

    Initial Commit

---
+++ ImageToGPS/ImageToGPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageToGPSApp.swift
+//  ImageToGPS
+//
+//  Created by Plamen Balinov on 26.08.26.
+//
+
+import SwiftUI
+
+@main
+struct ImageToGPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
